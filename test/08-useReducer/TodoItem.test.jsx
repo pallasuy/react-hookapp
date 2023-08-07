10b3ff05e10494a4524139e0a1614df461799dbd
gpallas <gpallas@metiri.la>
2023-08-06 23:53:29 -0300

    Pruebas botones en todoItem

--- test/08-useReducer/TodoItem.test.jsx
+++ test/08-useReducer/TodoItem.test.jsx
@@ -1,5 +1,5 @@
 import { TodoItem } from "../../08-useReducer/TodoItem";
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 
 describe('Pruebas sobre el <TodoItem />', () => {
 
@@ -39,11 +39,66 @@ describe('Pruebas sobre el <TodoItem />', () => {
         const spanElement = screen.getByLabelText('span');
         //console.log(spanElement.className);
         //screen.debug();
-        expect( spanElement.className.trim() ).toBe('align-self-center');
+        expect(spanElement.className.trim()).toBe('align-self-center');
+        expect(spanElement.className).not.toContain('text-decoration-line-through');
 
 
 
-    })
+    });
+
+    test('Debe de msotrar el todo completado', () => {
+
+        render(
+            <TodoItem
+                id={id}
+                description={description}
+                done={true}
+                onToggleTodo={onToggleTodoMock}
+                onDeleteTodoMock={onDeleteTodoMock}
+
+            />
+        );
+        const liElement = screen.getByRole('listitem');
+
+        expect(liElement.className).toBe("list-group-item d-flex justify-content-between");
+        const spanElement = screen.getByLabelText('span');
+        expect(spanElement.className).toContain('text-decoration-line-through');
+    });
+
+
+
+    test('Span debe llamar al ToggleTodo cuando se hace click', () => {
+        render(
+            <TodoItem
+                id={id}
+                description={description}
+                done={true}
+                onToggleTodo={onToggleTodoMock}
+                onDeleteTodoMock={onDeleteTodoMock}
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click(spanElement);
+        expect(onToggleTodoMock).toHaveBeenCalledWith(id);
+    });
+
+    test('Button debe llamar al deleteTodo cuando se hace click', () => {
+        render(
+            <TodoItem
+                id={id}
+                description={description}
+                done={true}
+                onToggleTodo={onToggleTodoMock}
+                onDeleteTodo={onDeleteTodoMock}
+            />
+        );
+
+        const buttonElement = screen.getByRole('button');
+        fireEvent.click( buttonElement );
+        expect( onDeleteTodoMock ).toHaveBeenCalledWith( id );
+    });
+
 
 
 
