f639a90ff423eb8472c0e8f0a0e565fd9da4d7d6
gpallas <gpallas@metiri.la>
2023-08-06 22:41:49 -0300

    Pruebas con mocks disparando eventos

--- test/03-examples/MultipleCustomHooks.test.jsx
+++ test/03-examples/MultipleCustomHooks.test.jsx
@@ -1,17 +1,77 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import { MultipleCustomHooks } from "../../03-examples/MultipleCustomHooks";
+import { useFetch } from "../../hooks/useFetch";
+import { useCounter } from "../../hooks/useCounter";
+
+jest.mock('../../hooks/useFetch');
+jest.mock('../../hooks/useCounter');
+
 
 describe('Pruebas en MultipleCustomHooks', () => {
+
+    const mockIncremente = jest.fn();
+    useCounter.mockReturnValue({
+        counter: 1,
+        increment: mockIncremente
+    });
+
+    beforeEach( ()=>{
+        jest.clearAllMocks();//Limpiamso los mocks antes de cada prueba
+    });
+
     test('Debe mostrar el componente por defecto', () => {
+
+        useFetch.mockReturnValue({
+            data: null,
+            isLoading: true,
+            hasError: null
+        });
+
         render(<MultipleCustomHooks />);
 
         expect(screen.getByText('Loading...'));
         expect(screen.getByText('Pokemon'));
 
-        const nextButton = screen.getByRole('button', { name: 'Next Pokemon' });    
+        const nextButton = screen.getByRole('button', { name: 'Next Pokemon' });
         expect(nextButton.disabled).toBeTruthy();
 
-        screen.debug();
 
     });
+
+
+    test('Debe de mostrar un Pokemon', () => {
+        const namePokemon = 'Pikachu';
+        useFetch.mockReturnValue({
+            data: { name: namePokemon, id: 1 },
+            isLoading: false,
+            hasError: null
+        });
+
+        render(<MultipleCustomHooks />);
+        //screen.debug();
+        expect(screen.getByText('Pokemon')).toBeTruthy();
+        expect(screen.getByText(namePokemon)).toBeTruthy();
+
+        const nextButton = screen.getByRole('button', { name: 'Next Pokemon' });
+        expect(nextButton.disabled).toBeFalsy();
+
+    });
+
+    test('Debe llamar la funcion de incrementar', () => {
+
+
+
+        const namePokemon = 'Pikachu';
+
+        render(<MultipleCustomHooks />);
+        const nextButton = screen.getByRole('button', { name: 'Next Pokemon' });
+        fireEvent.click(nextButton);
+
+        expect(mockIncremente).toHaveBeenCalled();
+
+    });
+
+
+
+
 });
